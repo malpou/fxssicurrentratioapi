32ee5361d43e19a7e7ecd75f28344bc297fd7b01
Malthe Poulsen <malthe@grundtvigsvej.dk>
2021-02-09 14:08:14 +0100

    Add cron job that runs every 12th hour

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
+import chron from "node-cron";
 import express from "express";
 import { getCurrentRatio } from "./data/getCurrentRatio";
 import { CurrentRatioResponse } from "./types";
+
+// Get data on server boot
+let data: CurrentRatioResponse | undefined;
+getCurrentRatio().then(res => {
+  data = res;
+});
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-
-let data: CurrentRatioResponse | undefined;
-
-getCurrentRatio().then(res => {
-  data = res;
-});
-
 
 app.get("/", async (_, res) => {
   res.json(data ? data : "No data awailable");
@@ -20,4 +20,9 @@ app.get("/", async (_, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
+});
+
+// Chron job to fetch data at 6 & 18 o'clock
+chron.schedule("0 6,18 * * *", async () => {
+  data = await getCurrentRatio();
 });
