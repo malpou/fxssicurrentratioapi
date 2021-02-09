58585a8e0383d89418fd8480d66aff3df8563e57
Malthe Poulsen <malthe@grundtvigsvej.dk>
2021-02-09 13:08:33 +0100

    Disable cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import { getCurrentRatio } from "./data/getCurrentRatio";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 1377;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.get("/", async (_, res) => {
